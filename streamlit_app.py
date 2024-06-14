2e28239bdf9febbe8f87e4013da1a49424a23a16
ihansel <1113998+ihansel@users.noreply.github.com>
2024-06-14 04:22:06 +0000

    Change Heading

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,8 +9,8 @@ import time
 import zipfile
 
 # Page title
-st.set_page_config(page_title='ML Model Building', page_icon='🤖')
-st.title('🤖 ML Model Building')
+st.set_page_config(page_title='MBS Demo App', page_icon='🤖')
+st.title('🤖 - MBS ML Model Building')
 
 with st.expander('About this app'):
   st.markdown('**What can this app do?**')
